a22e4e4f31c93632498787e2fa5a4a89ddd33717
Han Ning <hanning0511@gmail.com>
2024-04-10 21:26:14 +0800

    resources: rename hello_world.py to helloWorld.sh
    Signed-off-by: Han Ning <hanning0511@gmail.com>

--- vars/prepare.groovy
+++ vars/prepare.groovy
@@ -1,5 +1,5 @@
 def call() {
-	def script_python = libraryResource 'hello_world.py'
-	writeFile file: './hello_world.py', text: script_python
-	sh "python ./hello_world.py"
+	def script_content = libraryResource 'helloWorld.sh'
+	writeFile file: 'hello_world.sh', text: script_content
+	sh "bash hello_world.sh"
 }
